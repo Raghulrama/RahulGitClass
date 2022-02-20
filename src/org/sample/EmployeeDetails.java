da8c02ed0ab04909c1bfc5c22e3c8c9bd71e6578
Raghulrama <raghul.ramalingam19@gmail.com>
2022-02-20 11:06:33 +0530

    rahul done work

--- src/org/sample/EmployeeDetails.java
+++ src/org/sample/EmployeeDetails.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class EmployeeDetails {
+	private void m3() {
+		System.out.println("m3 created");
+	}
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
